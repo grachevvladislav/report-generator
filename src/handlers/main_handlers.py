8c9951239b8eb902fde79beea62a3ae616d9b64d
vlad <vladislav.grachev@gmail.com>
2024-02-06 14:35:46 +0300

    fix

--- src/handlers/main_handlers.py
+++ src/handlers/main_handlers.py
@@ -10,23 +10,21 @@ from telegram.ext import (
 
 from constants.keyboards import Buttons, start_keyboard
 from constants.states import States
-from google_sheet_backend import get_cashier_id, get_user_permissions
+from google_sheet_backend import get_user_permissions
 from handlers.report_generator import (
     counter_increase,
     make_report,
     wait_for_new_report,
 )
 
-from .notifications import add_pay_notifications, close_check
+from .notifications import close_check
 from .schedule import show_schedule
 
 
 async def start(update, context):
     context.user_data.update(get_user_permissions())
     client_id = str(update.effective_chat["id"])
-    if client_id == get_cashier_id():
-        return add_pay_notifications(update, context)
-    elif client_id in context.user_data["admins_dict"].keys():
+    if client_id in context.user_data["admins_dict"].keys():
         return await show_schedule(update, context)
     elif client_id in context.user_data["stuff_ids"]:
         await update.message.reply_text(
