59f428bfc44bc42503c9d75d093ee7adbaea43d5
grachevvladislav <vladislav.grachev@gmail.com>
2024-07-09 10:54:25 +0300

    fix dump

--- backend/core/crud.py
+++ backend/core/crud.py
@@ -47,7 +47,11 @@ def get_missing_dates(add_empty):
 def export_all_tables():
     buffer = io.StringIO()
     management.call_command(
-        "dumpdata --exclude auth.permission --exclude contenttypes",
+        "dumpdata",
+        "--exclude",
+        "auth.permission",
+        "--exclude",
+        "contenttypes",
         stdout=buffer,
     )
     buffer.seek(0)
