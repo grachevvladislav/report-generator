8dbe4fb8022dbcc8a8f3b141d419a94ea78e1ef7
grachevvladislav <vladislav.grachev@gmail.com>
2024-10-15 21:20:31 +0300

    fix logs

--- backend/bot/utils.py
+++ backend/bot/utils.py
@@ -1,5 +1,7 @@
 from telegram.error import BadRequest
 
+from backend.settings import logger
+
 PATTERN = "^{0}$"
 
 
@@ -7,8 +9,14 @@ async def send_or_edit_message(update, *args, **kwargs) -> None:
     query = update.callback_query
     if update.message:
         await update.message.reply_text(*args, **kwargs)
+        logger.info(
+            f'Sent message to {update.effective_chat["id"]}: {str(args)} {str(kwargs)}'
+        )
     else:
         try:
+            logger.info(
+                f'New message to {update.effective_chat["id"]}: {str(args)} {str(kwargs)}'
+            )
             await query.edit_message_text(*args, **kwargs)
         except BadRequest:
             pass
