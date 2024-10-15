bdba59277a69c80f54cc771ccc915fe43cecfd4a
grachevvladislav <vladislav.grachev@gmail.com>
2024-10-15 20:44:05 +0300

    add mkdir

--- pull_new_image.sh
+++ pull_new_image.sh
@@ -3,6 +3,7 @@ git pull
 docker compose stop
 docker compose down
 yes Y | docker system prune -a
+mkdir -p logs
 docker compose up -d
 docker exec -it report-generator-django sh -c "python manage.py collectstatic --no-input"
 docker exec -it report-generator-django sh -c "python manage.py migrate"
